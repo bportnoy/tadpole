7c3355165f593b1307549d01894d6fc6efbe720d
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-05-19 21:10:30 -0700

    Can now add, remove, and respawn children.
    Added several functions that allow us to respawn a child process in case it's killed prematurely, add additional child processes, and remove child proceses safely.

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ resources. In a multicore environment, we'll leave one core for Node and use the
 var _defaultOptions = {
   numChildren: require('os').cpus().length -1 || 1,
   priority: true,
-  addTimout: 30000
+  addTimout: 30000, 
+  env: process.ENV
 };
 
 var _functionDefaults = {
@@ -33,7 +34,7 @@ This object is mainly for holding the resolver functions, but also keeps a recor
  */
 var _inProcess = {};
 
-var _priorities = {};
+var _functions = {};
 
 var _queue = new PriorityQueue({comparator: function(a,b){ return a.priority - b.priority;}});
 
@@ -46,34 +47,20 @@ var Tadpole = {
 
     userOptions = userOptions || {};
     _.defaults(userOptions, _defaultOptions);
+    _options = userOptions;
 
     for (var i = 0; i < userOptions.numChildren; i++){
-
-      var id = uuid.v4();
-      var child = cp.fork(__dirname + '/child.js', {execArgv: []});
-
-      child.on('error', function(error){
-        console.error('Error in child process: ' + error);
-      });
-
-      //when we get the result of reach transaction, this function is called to get the next operation in the queue
-      child.on('message', function(payload){
-        next(id, payload);
-      });
-
-      var container = {thread: child, childId: id, active: false};
-      _children[id] = container;
+      addChild();
     }
 
-    _options = userOptions;
     _initialized = true;
 
   },
 
-  add: function(functionObject) {
+  addFunction: function(functionObject) {
 
     if (_addsPending > 0) throw new Error('Cannot add another function until previous add has completed.');
-    if (_priorities[functionObject.name]) throw new Error('Already added a function with that name.');
+    if (_functions[functionObject.name]) throw new Error('Already added a function with that name.');
     if (functionObject.priority < 1) throw new Error('Priority less than 1 reserved for module use.');
     if (!functionObject.func || !functionObject.name) throw new Error('Function object must contain both function and name.');
 
@@ -83,19 +70,10 @@ var Tadpole = {
 
     functionObject.action = 'ADDFUNC';
 
-    _priorities[functionObject.name] = functionObject.priority;
+    _functions[functionObject.name] = functionObject;
 
     _.each(_children, function(child){
-      _addsPending++;
-      setId(functionObject);
-      new Promise(function(resolve, reject) {
-        functionObject.resolver = resolve;
-        functionObject.rejecter = reject;
-        _inProcess[functionObject.id] = functionObject;
-        child.thread.send(functionObject);
-        // console.log('IP', _inProcess[functionObject.id]);
-      });
-
+      injectFunction(functionObject, child);
     });
 
     return new Promise(function(resolve, reject){
@@ -107,7 +85,7 @@ var Tadpole = {
   run: function(name, args){
     return new Promise(function(resolve, reject){
       request({action: name,
-                priority: _priorities[name],
+                priority: _functions[name].priority,
                 args: args,
                 resolver: resolve,
                 rejecter: reject
@@ -118,7 +96,18 @@ var Tadpole = {
 
   remove: function(num){
     num = num || 1;
-    _toRemove++;
+    while(num){
+      _toRemove++;
+      if(!_queue.length) killChild();
+      num--;
+    }
+  },
+
+  add: function(num){
+    while(num){
+      restoreChildFunctions(addChild());
+      num--;
+    }
   },
 
   size: function(){
@@ -159,8 +148,7 @@ function next (childId, payload) {
 
   //take this child out of service if we want to decrement running processes
   if (_toRemove) {
-    delete children[childId];
-    _toRemove--;
+    killChild(childId);
     return;
   }
 
@@ -196,4 +184,67 @@ function request (payload){
     });
     if (!slotted) _queue.queue(payload); //in case the queue was empty, but all the threads were working
   }
+}
+
+function addChild(id){
+  id = id || uuid.v4();
+  var child = cp.fork(__dirname + '/child.js', {env: _options.env, execArgv: []});
+
+  child.on('error', function(error){
+    console.error('Error in child process: ' + error);
+  });
+
+  //when we get the result of reach transaction, this function is called to get the next operation in the queue
+  child.on('message', function(payload){
+    next(id, payload);
+  });
+
+  child.on('exit', function(code, signal){
+    if (!signal === 'SIGTERM'){
+      addChild(id);
+      restoreChildFunctions(child);
+    }
+  });
+
+  var container = {thread: child, id: id, active: false};
+  _children[id] = container;
+  return id;
+}
+
+function injectFunction(functionObject, child){
+  _addsPending++;
+  setId(functionObject);
+  new Promise(function(resolve, reject) {
+    functionObject.resolver = resolve;
+    functionObject.rejecter = reject;
+    _inProcess[functionObject.id] = functionObject;
+    child.thread.send(functionObject);
+  });
+}
+
+function restoreChildFunctions(child){
+  if (!_addsPending){
+    _.each(_functions, function(functionObject){
+      injectFunction(functionObject, child);
+    });
+  } else setTimeout(function(){
+    restoreChildFunctions(child);
+  }, 500);
+}
+
+function killChild(childId){
+  if (childId){
+    _children[childId].thread.kill();
+    delete _children[childId];
+    _toRemove--; 
+  } else {
+      _.each(_children, function(child){
+        if (!child.active){
+          killChild(child.id);
+          _toRemove--;
+          return false; //we can stop once we get one
+        }
+      //we haven't killed the child, but there is still a value in _toRemove, so it should be killed when it finishes what it's working on
+      });
+    }
 }
